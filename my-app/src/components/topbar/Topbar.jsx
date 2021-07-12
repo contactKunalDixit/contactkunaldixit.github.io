4919ac0e2554e93c05b1ac79e1f30dbff2d6d6b2
Kunal Dixit <contactkunaldixit@gmail.com>
2021-07-12 15:04:32 +0800

    Updated topbar for logo title

--- my-app/src/components/topbar/Topbar.jsx
+++ my-app/src/components/topbar/Topbar.jsx
@@ -9,7 +9,7 @@ function Topbar(props) {
 			<div className='wrapper'>
 				<div className='left'>
 					<a href='#intro' className='logo'>
-						genius.
+						contactkunaldixit.github.io
 					</a>
 					<div className='itemContainer'>
 						<Call className='icon'></Call>
